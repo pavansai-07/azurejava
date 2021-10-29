a2469ba92fa8f14ff9f9e2939132ec402d0ed852
PavanSaiGontina <62195072+pavansai-07@users.noreply.github.com>
2021-10-29 09:49:19 +0530

    Update pavan.java

--- pavan.java
+++ pavan.java
@@ -1,13 +1,9 @@
 class pavan {
-
   public static void main(String[] args) {
-    
     System.out.println("Enter two numbers");
     int first = 10;
     int second = 20;
-    
     System.out.println(first + " " + second);
-
     // add two numbers
     int sum = first + second;
     System.out.println("The sum is: " + sum);
